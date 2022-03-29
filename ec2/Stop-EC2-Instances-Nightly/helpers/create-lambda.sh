5564b65ba6d336edaf48850fedc3597f643b3113
Aissa Laribi <57592948+aissa-laribi@users.noreply.github.com>
2022-03-29 17:30:59 +0100

    Update and rename lambda.sh to create-lambda.sh

--- ec2/Stop-EC2-Instances-Nightly/helpers/create-lambda.sh
+++ ec2/Stop-EC2-Instances-Nightly/helpers/create-lambda.sh
@@ -1,5 +1,6 @@
 curl https://raw.githubusercontent.com/aissa-laribi/awsops/main/ec2/Stop-EC2-Instances-Nightly/lambda_function.py > lambda_function.py
 #Replace 'Enter_Region_Name' with your region
 zip -9 lambda_function.zip lambda_function.py
-aws lambda create-function --function-name ec2-stop  --role arn:aws:iam::238029455348:role/LambdaEC2StartStop \
---handler lambda_function.lambda_handler --runtime python3.7 --timeout 2 0 --zip-file fileb://lambda_function.zip
+
+aws lambda create-function --function-name ec2-stop  --role <lambda-arn> \
+--handler lambda_function.lambda_handler --runtime python3.7 --timeout 120 --zip-file fileb://lambda_function.zip

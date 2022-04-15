fb54709dde66588134fc83c73db1302ed5f07e0d
aissa-laribi <aissalaribi@yahoo.fr>
2022-04-15 19:29:12 +0100

    Adding ConvertToWebp

--- S3/Converting-Images/helpers/create-lambda.sh
+++
@@ -1,6 +0,0 @@
-curl https://raw.githubusercontent.com/aissa-laribi/awsops/main/S3/Converting-Images/lambda_function.py > lambda_function.py
-#Replace 'Enter_Region_Name' with your region
-zip -r9 lambda_function.zip wand lambda_function.py
-aws iam list-roles | grep ConvertToWebp
-aws lambda create-function --function-name ConvertToWebp  --role arn:aws:iam::133631580572:role/ConvertToWebp \
---handler lambda_function.lambda_handler --runtime python3.8 --timeout 300 --memory 512 --zip-file fileb://lambda_function.zip
